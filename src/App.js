a62ffe492e6033956eba3d817a34ae589c0bd060
unknown <obrienules@gmail.com>
2019-11-26 04:16:52 +0100

    Add reacr router wrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import HomeUi from './Components/Home/HomeUi'
+import About from './Components/About/AboutUi';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+
+render(){
+  const { width } = this.state;
   return (
     <div className="App">
-      <HomeUi />
+      <HomeUi width = {width} />
+      <About width = {width} />
     </div>
   );
+}
+ 
 }
 
 export default App;
